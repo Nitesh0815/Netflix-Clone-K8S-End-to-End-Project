c15f8c4e56ac25b5b13097956bbddc08fddabffe
DestructiveSoul15 <nk9910922101@gmail.com>
2025-10-29 20:18:47 +0530

    i think this is the one correct file

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.ubuntu.id
   subnet_id              = aws_subnet.public_subnet[count.index].id
   instance_type          = var.ec2_instance_type[count.index]
+  key_name               = var.ssh_key_name
   iam_instance_profile   = aws_iam_instance_profile.iam_instance_profile.name
   vpc_security_group_ids = [aws_security_group.default_ec2_sg.id]
 
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -14,3 +14,9 @@ variable "ec2_instance_type" {
 }
 variable "ec2_volume_size" {}
 variable "ec2_volume_type" {}
+
+# New variable required for Ansible connectivity via GitHub Actions secret
+variable "ssh_key_name" {
+  description = "The name of the AWS Key Pair to use for SSH access to EC2 instances. Value is typically passed via the TF_VAR_ssh_key_name environment variable from GitHub Actions."
+  type        = string
+}
