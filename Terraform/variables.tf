61866864254c491a622597b62dfe3c4c48168f35
DestructiveSoul15 <nk9910922101@gmail.com>
2025-10-30 13:21:59 +0530

    fixing .yml

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,19 +1,52 @@
-variable "aws-region" {}
-variable "env" {}
-variable "cidr-block" {}
-variable "pub-subnet-count" {}
+variable "aws-region" {
+  description = "The AWS region to deploy resources in."
+  type        = string
+}
+
+variable "env" {
+  description = "The deployment environment (e.g., dev, staging, prod)."
+  type        = string
+}
+
+variable "cidr-block" {
+  description = "The CIDR block for the VPC."
+  type        = string
+}
+
+variable "pub-subnet-count" {
+  description = "Number of public subnets to create."
+  type        = number
+}
+
 variable "pub-cidr-block" {
-  type = list(string)
+  description = "List of CIDR blocks for the public subnets."
+  type        = list(string)
 }
+
 variable "pub-availability-zone" {
-  type = list(string)
+  description = "List of Availability Zones for the public subnets."
+  type        = list(string)
 }
-variable "ec2-instance-count" {}
+
+variable "ec2-instance-count" {
+  description = "Number of EC2 instances to create."
+  type        = number
+}
+
 variable "ec2_instance_type" {
-  type = list(string)
+  description = "List of instance types for EC2 instances."
+  type        = list(string)
 }
-variable "ec2_volume_size" {}
-variable "ec2_volume_type" {}
+
+variable "ec2_volume_size" {
+  description = "The size of the root volume in GB."
+  type        = number
+}
+
+variable "ec2_volume_type" {
+  description = "The type of the root volume (e.g., gp3)."
+  type        = string
+}
 
 # NEW: Variable definition for the SSH Public Key
 variable "ssh_public_key" {
