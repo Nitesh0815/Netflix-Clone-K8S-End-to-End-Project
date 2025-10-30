762f28ebf5810acbdfa2ede6d0ccbd4b650deee5
DestructiveSoul15 <nk9910922101@gmail.com>
2025-10-30 11:31:32 +0530

    fixing

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,16 +1,21 @@
-# 1. AWS KEY PAIR RESOURCE
-# This creates the key pair in AWS using the public key passed via GitHub secrets.
 resource "aws_key_pair" "deployer_key" {
   # Key name is constructed using locals defined in vpc.tf
-  key_name   = "${local.org}-${local.project}-${local.env}-key"
+  key_name = "${local.org}-${local.project}-${local.env}-key"
   # This variable must be passed from the GitHub Action secret SSH_PUBLIC_KEY
   public_key = var.ssh_public_key
 }
 
-# 2. OUTPUT INSTANCE PUBLIC IPS
-# This output is crucial. The GitHub Actions workflow (Ansible job) will
-# read this JSON list to dynamically generate the inventory file.
-output "instance_public_ips" {
-  description = "Public IP addresses of the EC2 instances for Ansible connectivity."
-  value       = aws_instance.ec2[*].public_ip
+output "public_ip_addresses" {
+  description = "The public IP addresses of the created EC2 instances"
+  value       = aws_instance.ec2.*.public_ip
+}
+
+output "instance_names" {
+  description = "The names of the created EC2 instances"
+  value       = aws_instance.ec2.*.tags.Name
+}
+
+output "ssh_key_name" {
+  description = "The name of the SSH key pair created"
+  value       = aws_key_pair.deployer_key.key_name
 }
