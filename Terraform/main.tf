ff73d3aa3d224cba02adc066c449e82df102dcea
DestructiveSoul15 <nk9910922101@gmail.com>
2025-10-31 21:59:40 +0530

    Fixing ansible.yml 6 time

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,26 +1,26 @@
 locals {
   instance_names = [
-    "jenkins_server",
-    "monitoring_server",
-    "kubernetes_master_node",
-    "kubernetes_worker_node"
+    "jenkins-server",
+    "monitoring-server",
+    "kubernetes-master-node",
+    "kubernetes-worker-node"
   ]
 }
 
 resource "aws_instance" "ec2" {
   count                  = var.ec2-instance-count
   ami                    = data.aws_ami.ubuntu.id
-  subnet_id              = aws_subnet.public_subnet[count.index].id
+  subnet_id              = aws_subnet.public-subnet[count.index].id
   instance_type          = var.ec2_instance_type[count.index]
-  iam_instance_profile   = aws_iam_instance_profile.iam_instance_profile.name
-  vpc_security_group_ids = [aws_security_group.default_ec2_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.iam-instance-profile.name
+  vpc_security_group_ids = [aws_security_group.default-ec2-sg.id]
 
   root_block_device {
     volume_size = var.ec2_volume_size
     volume_type = var.ec2_volume_type
   }
 
-  # ✅ Ensure SSM Agent is running
+  # ✅ Install and start SSM Agent automatically
   user_data = <<-EOF
               #!/bin/bash
               apt update -y
@@ -31,11 +31,9 @@ resource "aws_instance" "ec2" {
               EOF
 
   tags = {
-    Name    = "${local.org}-${local.project}-${local.env}-${local.instance_names[count.index]}"
-    Role    = local.instance_names[count.index]
-    Env     = local.env
-    Project = "Netflix"
+    Name = "${local.org}-${local.project}-${local.env}-${local.instance_names[count.index]}"
+    Env  = "${local.env}"
   }
 
-  depends_on = [aws_iam_instance_profile.iam_instance_profile]
+  depends_on = [aws_iam_instance_profile.iam-instance-profile]
 }
